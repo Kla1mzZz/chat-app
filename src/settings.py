946ac831642921f07011108765711868a48b496d
Kla1mzZ <vovka123osipok@gmail.com>
2024-12-10 18:49:00 +0200

    added handy settings

--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings
+from pydantic import BaseModel
+
+class DatabaseConfig(BaseModel):
+    database_url: str
+
+class JwtConfig(BaseModel):
+    secret_key: str
+    algorithm: str = 'HS256'
+    access_token_expire_minutes: int = 60
 
 class Settings(BaseSettings):
-    database_url: str
-    secret_key: str
+    database: DatabaseConfig
+    jwt: JwtConfig
     
     class Config:
         env_file = '.env'
+        env_nested_delimiter='__'
 
 settings = Settings()
